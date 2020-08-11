04701f90bf17dd07d4b7b95c1150fd0b786e99f5
Tuukka Koistinen <tvkoisti@gmail.com>
2020-08-11 17:58:42 +0300

    add install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+root_dir=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
+raspbian_image="${root_dir}"/raspbian/2020-02-13-raspbian-buster.zip
+memory_card_dev=/dev/mmcblk0
+
+unzip -p "${raspbian_image}" | sudo dd of="${memory_card_dev}" bs=4M conv=fsync
