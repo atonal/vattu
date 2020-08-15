794a4874e71db7c54c546a63f0bbf407baa86cdc
Tuukka Koistinen <tvkoisti@gmail.com>
2020-08-15 10:12:04 +0300

    install: add error boilerplate

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 root_dir=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 raspbian_image="${root_dir}"/raspbian/2020-02-13-raspbian-buster.zip
 memory_card_dev=/dev/mmcblk0
