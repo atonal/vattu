154aa5c10b9e9467351a8d2800a8484cea632185
Tuukka Koistinen <tvkoisti@gmail.com>
2020-08-18 16:14:55 +0300

    configure: add ssh public key to the filesystem

--- configure.sh
+++ configure.sh
@@ -3,8 +3,16 @@
 set -euo pipefail
 
 boot_partition=/dev/mmcblk0p1
+filesys_partition=/dev/mmcblk0p2
 mount_dir=/mnt/vattu
+user=${USER}
 
 sudo mount -v "${boot_partition}" "${mount_dir}"
 sudo cp -v conf-files/* "${mount_dir}"/
+sudo umount -v "${mount_dir}"
+
+sudo mount -v "${filesys_partition}" "${mount_dir}"
+sudo mkdir -v -p -m 700 "${mount_dir}"/home/pi/.ssh
+sudo cp -v /home/"${user}"/.ssh/id_rsa.pub "${mount_dir}"/home/pi/.ssh/authorized_keys
+sudo chown -v -R 1000:1000 "${mount_dir}"/home/pi/.ssh
 sudo umount -v "${mount_dir}"
