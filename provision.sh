ac46c1d5cf971e1f24a00e2afd0ad4d42ee5a6cd
Tuukka Koistinen <tvkoisti@gmail.com>
2020-08-23 11:55:36 +0300

    provision: pass command line variables to ansible command

--- provision.sh
+++ provision.sh
@@ -2,4 +2,4 @@
 
 root_dir=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
 
-ansible-playbook "${root_dir}"/ansible/playbook.yml -i "${root_dir}"/ansible/inventory
+ansible-playbook "${root_dir}"/ansible/playbook.yml -i "${root_dir}"/ansible/inventory "$@"
