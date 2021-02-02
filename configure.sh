54b8261593325c36dbfc3d71a60add2439a06204
Tuukka Koistinen <tvkoisti@gmail.com>
2021-02-02 19:11:43 +0200

    config: edit boot cmd to include cgroup_memory
    Also mount the partitions under the current user with the help of
    bindfs.

--- configure.sh
+++ configure.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 boot_partition=/dev/mmcblk0p1
 filesys_partition=/dev/mmcblk0p2
 mount_dir=/mnt/vattu
+tmp_dir=$(mktemp -d)
 user=${USER}
 
 unmount_boot_partition()
@@ -15,12 +16,13 @@ unmount_boot_partition()
 
 mount_boot_partition()
 {
-    sudo mount -v "${boot_partition}" "${mount_dir}"
+    sudo mount -v -o uid="$(id -u)",gid="$(id -g)" "${boot_partition}" "${mount_dir}"
     trap unmount_boot_partition EXIT
 }
 
 unmount_filesys_partition()
 {
+    sudo umount -v "${tmp_dir}" || echo umount failed
     sudo umount -v "${mount_dir}"
     trap EXIT
 }
@@ -28,57 +30,69 @@ unmount_filesys_partition()
 mount_filesys_partition()
 {
     sudo mount -v "${filesys_partition}" "${mount_dir}"
+    sudo bindfs -u "$(id -u)" -g "$(id -g)" "${mount_dir}" "${tmp_dir}"
     trap unmount_filesys_partition EXIT
 }
 
 copy_conf_files()
 {
-    sudo cp -v conf-files/* "${mount_dir}"/
+    cp -v conf-files/* "${mount_dir}"/
+}
+
+edit_boot_cmd()
+{
+    if ! grep -q "cgroup_memory" "${mount_dir}"/cmdline.txt ; then
+        sed -i 's/$/ cgroup_memory=1/' "${mount_dir}"/cmdline.txt
+    fi
+    if ! grep -q "cgroup_enable" "${mount_dir}"/cmdline.txt ; then
+        sed -i 's/$/ cgroup_enable=memory/' "${mount_dir}"/cmdline.txt
+    fi
 }
 
 create_wheel_group()
 {
     if ! grep -q "wheel:" "${mount_dir}"/etc/group ; then
-        sudo groupadd -R "${mount_dir}" wheel
+        groupadd -R "${mount_dir}" wheel
     fi
 }
 
 add_user()
 {
-    if ! grep -q "${user}:" "${mount_dir}"/etc/passwd ; then
-        sudo useradd -m -R "${mount_dir}" "${user}"
+    if ! grep -q "${user}:" "${tmp_dir}"/etc/passwd ; then
+        useradd -m -R "${tmp_dir}" "${user}"
     fi
 }
 
 copy_ssh_key()
 {
-    sudo mkdir -v -p -m 700 "${mount_dir}"/home/"${user}"/.ssh
-    sudo cp -v /home/"${user}"/.ssh/id_rsa.pub "${mount_dir}"/home/"${user}"/.ssh/authorized_keys
-    uid=$(grep -m 1 "${user}" "${mount_dir}"/etc/passwd | cut -d ':' -f 3)
-    gid=$(grep -m 1 "${user}" "${mount_dir}"/etc/passwd | cut -d ':' -f 4)
-    sudo chown -v -R "${uid}":"${gid}" "${mount_dir}"/home/"${user}"/.ssh
+    mkdir -v -p -m 700 "${tmp_dir}"/home/"${user}"/.ssh
+    cp -v /home/"${user}"/.ssh/id_rsa.pub "${tmp_dir}"/home/"${user}"/.ssh/authorized_keys
+    uid=$(grep -m 1 "${user}" "${tmp_dir}"/etc/passwd | cut -d ':' -f 3)
+    gid=$(grep -m 1 "${user}" "${tmp_dir}"/etc/passwd | cut -d ':' -f 4)
+    sudo chown -v -R "${uid}":"${gid}" "${tmp_dir}"/home/"${user}"/.ssh
 }
 
 disable_pi_password()
 {
-    sudo sed -i 's/\(pi:\)[^:]*\(:.*\)/\1!\2/' "${mount_dir}"/etc/shadow
+    sed -i 's/\(pi:\)[^:]*\(:.*\)/\1!\2/' "${tmp_dir}"/etc/shadow
 }
 
 add_user_to_wheel()
 {
-    sudo sed -i 's/\(^wheel.*:\).*/\1'"${user}"'/' "${mount_dir}"/etc/group
+    sed -i 's/\(^wheel.*:\).*/\1'"${user}"'/' "${tmp_dir}"/etc/group
 }
 
 nopasswd_sudo_for_wheel()
 {
-    if ! sudo grep -q '%wheel ALL=(ALL) NOPASSWD: ALL' "${mount_dir}"/etc/sudoers ; then
-        echo '%wheel ALL=(ALL) NOPASSWD: ALL' | sudo tee -a "${mount_dir}"/etc/sudoers > /dev/null
-        sudo visudo -cf "${mount_dir}"/etc/sudoers
+    if ! grep -q '%wheel ALL=(ALL) NOPASSWD: ALL' "${tmp_dir}"/etc/sudoers ; then
+        echo '%wheel ALL=(ALL) NOPASSWD: ALL' >> "${tmp_dir}"/etc/sudoers > /dev/null
+        visudo -cf "${tmp_dir}"/etc/sudoers
     fi
 }
 
 mount_boot_partition
 copy_conf_files
+edit_boot_cmd
 unmount_boot_partition
 
 mount_filesys_partition
